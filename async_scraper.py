794248a4a30aeba31e13601650f3e64a26cca3e7
aquaracer <50531938+aquaracer@users.noreply.github.com>
2019-11-10 23:27:19 +0500

    Update async_scraper.py

--- async_scraper.py
+++ async_scraper.py
@@ -14,15 +14,15 @@ async def pars_url():
     browser = browsers.Firefox()
 
     async with get_session(service, browser) as session:
-        await session.get("https://moikrug.ru/vacancies?q=3d+s+max&sort=date&currency=rur&remote=1")
-        list_of_url = await session.get_elements('a[class=job_icon]')  # // await
+        await session.get("https://moikrug.ru/vacancies?q=3d+s+max&sort=date&currency=rur&remote=1") # открыываем странице с запросом на вакансию
+        list_of_url = await session.get_elements('a[class=job_icon]')  # получаем массив элемнтов с именем класса 'job_icon'
+        final_list_of_urls = []
         for item in list_of_url:
             element_2 = item.get_text()
-            print('dir(element_2)', dir(element_2))
-            print('type item', type(item))
-            print('element_2', element_2)
-            print('type element_2', type(element_2))
-
+            print('type item', type(item))  # type item <class 'arsenic.session.Element'>
+            print('element_2', element_2) #  element_2 <coroutine object Element.get_text at 0x10b473cc8>
+            print('type element_2', type(element_2)) # type element_2 <class 'coroutine'>
+            final_list_of_urls.append(element_2)
 
 def main():
     loop = asyncio.get_event_loop()
